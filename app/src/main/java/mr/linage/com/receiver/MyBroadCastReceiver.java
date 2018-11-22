096cea34d988aea07e9519216b5a0be7baff800c
여대현 <huntux2@jyes.co.kr>
2018-11-22 11:43:45 +0900

    gogo

---
+++ app/src/main/java/mr/linage/com/receiver/MyBroadCastReceiver.java
@@ -0,0 +1,14 @@
+package mr.linage.com.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadCastReceiver  extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String actionName = intent.getAction();
+        Toast.makeText(context, "받은 액션 : "+actionName , Toast.LENGTH_SHORT).show();
+    }
+}
